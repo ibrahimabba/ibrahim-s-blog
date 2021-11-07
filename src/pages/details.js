7a330162f9ba3d36c42b1f4b0dc2f0b3db30e94a
ibrahimabba <ibrahimsuleiman364@gmail.com>
2021-11-08 00:44:36 +0100

    props fix

--- src/pages/details.js
+++ src/pages/details.js
@@ -1,16 +1,24 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 
 const createdStyle = { fontSize: 12 };
 const detailStyle = { marginTop: 40 };
 
 const BlogDetail = ({ location }) => {
+  const [post, setPost] = useState();
+
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+    setPost(location.state.post);
+  }, []);
   return (
     <Layout pageTitle="">
       <div>
-        <h3>{location.state.post.title}</h3>
-        <h6 style={createdStyle}>{location.state.post.created}</h6>
-        <p style={detailStyle}>{location.state.post.details}</p>
+        <h3>{post?.title}</h3>
+        <h6 style={createdStyle}>{post?.created}</h6>
+        <p style={detailStyle}>{post?.details}</p>
       </div>
     </Layout>
   );
