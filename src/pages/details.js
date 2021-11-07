7a62985e94afa00cad73cafb47a270773c57afdc
ibrahimabba <ibrahimsuleiman364@gmail.com>
2021-11-08 00:33:38 +0100

    .

--- src/pages/details.js
+++ src/pages/details.js
@@ -5,13 +5,12 @@ const createdStyle = { fontSize: 12 };
 const detailStyle = { marginTop: 40 };
 
 const BlogDetail = ({ location }) => {
-  const post = location.state.post;
   return (
     <Layout pageTitle="">
       <div>
-        <h3>{post.title}</h3>
-        <h6 style={createdStyle}>{post.created}</h6>
-        <p style={detailStyle}>{post.details}</p>
+        <h3>{location.state.post.title}</h3>
+        <h6 style={createdStyle}>{location.state.post.created}</h6>
+        <p style={detailStyle}>{location.state.post.details}</p>
       </div>
     </Layout>
   );
